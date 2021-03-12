ba42e7b51045fc63cd1c8e7053247487c506bca5
AvinashxU <imasy36@gmail.com>
2021-03-12 18:04:31 +0530

    created menu

--- todo.py
+++ todo.py
@@ -1,3 +1,11 @@
 ## todo.py
+import sys
+
+def menu():
+	print("Welcome . . . \n1.Add \n2.Update \n3.Remove \n4.exit")
+	cho = input("choice -: ")
+	if cho==1:
+		print()
+
 if __name__=='__main__':
 	pass
