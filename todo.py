f2ae4654b2dc1f982d055a8a976f4a44e8432e59
AvinashxU <imasy36@gmail.com>
2021-03-16 15:45:29 +0530

    decide how to use CLI parameters

--- todo.py
+++ todo.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import sqlite3
 
+version = 1.0
 class todo:
 	def __init__(self, directory, filename):
 		self.filename = filename
@@ -35,30 +36,45 @@ class todo:
 
 	def remove(self, data):
 		self.cur.execute('DELETE FROM Tasks WHERE id={};'.format(data[0]))
+		print('Task with Id:{} deleted successfully')
 		return
 
 	def changePriority(self, data):
 		self.cur.execute('UPDATE Tasks SET priority={} WHERE id={}'.format(data[1],data[0]))
 		return
 
+	def close(self):
+		self.cur.commit()
+		self.conn.close()
 
 if __name__=='__main__':
 	parser = argparse.ArgumentParser(prog='ToDo', description='Todo CLI tool - to keep your schedules',
 	 epilog='Enjoy your day:-)', fromfile_prefix_chars='@')
 	
-	parser.add_argument('-a',nargs=1, type=str, help='Add a new task')
-	parser.add_argument('-cp', nargs=2, type=int, help='Change priority of task - id, new priority')
-	parser.add_argument('-r',nargs=1, type=int, help='Remove a task')
-	parser.add_argument('-l', action='store_true', help='list tasks detail')	
+	parser.add_argument('-a', '--add', action='store_true', help='Add a task')
+	parser.add_argument('-r', '--remove', action='store_true', help='Remove a task')
+	parser.add_argument('-cp', '--changePriority', action='store_true', help='Change priority of a task')
+	parser.add_argument('-l', '--list', action='store_true', help='list tasks detail')
+	parser.add_argument('-ve', '--version', action='store_true', help='version of ToDO')	
+	parser.add_argument('vars', nargs='*', help='Additional required details')
 	args = parser.parse_args()
 	
 	obj = todo('data','list.sqlite3')
-	print(vars(args))
 
-	if args.l:
+	if args.version:
+		print('ToDo : Version {}\n'.format(version))
+
+	if args.list:
 		obj.list()
 
-	if args.a:
-		obj.add()
+	if args.add:
+		obj.add(args.vars)
+
+	if args.changePriority:
+		obj.changePriority(args.vars)
+
+	if args.remove:
+		obj.remove(args.vars)
+
 	
 
